cfa04b75156ddfd761710a961a8c223e5c387f79
mahadihasan2 <mahadihasan125643@gmail.com>
2022-04-13 19:55:11 +0600

    useParams-set

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home/Home';
 import About from './pages/About/About';
 import Header from './pages/Shared/Header/Header';
 import Footer from './pages/Shared/Footer/Footer';
+import ServiceDetail from './pages/ServiceDetails/ServiceDetail';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/about' element={<About></About>}></Route>
       </Routes>
       <Footer></Footer>
--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css'
 const Service = ({service}) => {
-    const {name,img,description,price} = service
+    const {id,name,img,description,price} = service
+    const navigate = useNavigate()
+    const handleNavigateToServiceDetails = id =>{
+            navigate(`/service/${id}`)
+    }
     return (
         <div className='service-container'>
             <img src={img} alt="" />
             <h2>Name: {name}</h2>
             <p>Price: {price}</p>
             <p>{description}</p>
-            <button>Book: {name}</button>
+            <button onClick={()=>handleNavigateToServiceDetails(id)}>Book: {name}</button>
         </div>
     );
 };
